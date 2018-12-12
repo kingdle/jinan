e4f60a20c33c43e0e019de6eab546f1fc50ad01b
kingdle <nkings@163.com>
2018-12-13 01:56:08 +0800

    ppt v2.0

---
+++ public/ppt/js/chartsBar.js
@@ -0,0 +1,67 @@
+/**
+ * ---------------------------------------
+ * This demo was created using amCharts 4.
+ *
+ * For more information visit:
+ * https://www.amcharts.com/
+ *
+ * Documentation is available at:
+ * https://www.amcharts.com/docs/v4/
+ * ---------------------------------------
+ */
+
+// Themes begin
+am4core.useTheme(am4themes_animated);
+// Themes end
+
+// Create chart instance
+var chart = am4core.create("chartsBars", am4charts.XYChart3D);
+chart.paddingBottom = 30;
+chart.angle = 35;
+
+// Add data
+chart.data = [{
+    "country": "单位",
+    "visits": 160816
+}, {
+    "country": "法人单位",
+    "visits": 145763
+}, {
+    "country": "产业单位",
+    "visits": 14656
+}, {
+    "country": "个体户",
+    "visits": 409899
+}];
+
+// Create axes
+var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+categoryAxis.dataFields.category = "country";
+categoryAxis.renderer.grid.template.location = 0;
+categoryAxis.renderer.minGridDistance = 20;
+categoryAxis.renderer.inside = true;
+categoryAxis.renderer.grid.template.disabled = true;
+
+let labelTemplate = categoryAxis.renderer.labels.template;
+labelTemplate.rotation = -90;
+labelTemplate.horizontalCenter = "left";
+labelTemplate.verticalCenter = "middle";
+labelTemplate.dy = 10; // moves it a bit down;
+labelTemplate.inside = false; // this is done to avoid settings which are not suitable when label is rotated
+
+var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+valueAxis.renderer.grid.template.disabled = true;
+
+// Create series
+var series = chart.series.push(new am4charts.ConeSeries());
+series.dataFields.valueY = "visits";
+series.dataFields.categoryX = "country";
+
+var columnTemplate = series.columns.template;
+columnTemplate.adapter.add("fill", (fill, target) => {
+    return chart.colors.getIndex(target.dataItem.index);
+})
+
+columnTemplate.adapter.add("stroke", (stroke, target) => {
+    return chart.colors.getIndex(target.dataItem.index);
+})
