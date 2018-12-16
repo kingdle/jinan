cbeda11e15b75b87e62f467aa7f81b274520b67c
kingdle <nkings@163.com>
2018-12-17 01:01:27 +0800

    update big ppt

--- public/ppt/js/chartsBar.js
+++ public/ppt/js/chartsBar.js
@@ -16,53 +16,186 @@ am4core.useTheme(am4themes_dark);
 // Themes end
 
 // Create chart instance
-var chart = am4core.create("chartsBars", am4charts.XYChart3D);
+var chart = am4core.create("chartsBars", am4charts.XYChart);
 chart.paddingBottom = 30;
 chart.angle = 35;
 
 // Add data
+
+
+
 chart.data = [{
-    "country": "单位",
-    "visits": 160816
-}, {
-    "country": "法人单位",
-    "visits": 145763
-}, {
-    "country": "产业单位",
-    "visits": 14656
-}, {
-    "country": "个体户",
-    "visits": 409899
-}];
+    "ten_fee": 44.94,
+    "fee": 1800.38,
+    "item_sum": 26,
+    "item_sum_all": 45,
+    "item_start": 11,
+    "item_finish": 8,
+    "district": "微山县",
+    "pic": "https://images.qdbfg.com/jining/weishan.png",
+    "id": 9
+},
+    {
+        "ten_fee": 32.78,
+        "fee": 1800.38,
+        "item_sum": 11,
+        "item_sum_all": 18,
+        "item_start": 6,
+        "item_finish": 1,
+        "district": "鱼台县",
+        "pic": "https://images.qdbfg.com/jining/yutai.png",
+        "id": 11
+    },
+    {
+        "ten_fee": 26.63,
+        "fee": 1800.38,
+        "item_sum": 23,
+        "item_sum_all": 35,
+        "item_start": 10,
+        "item_finish": 2,
+        "district": "兖州区",
+        "pic": "https://images.qdbfg.com/jining/yanzhou.png",
+        "id": 3
+    },
+    {
+        "ten_fee": 25.93,
+        "fee": 1800.38,
+        "item_sum": 27,
+        "item_sum_all": 49,
+        "item_start": 13,
+        "item_finish": 6,
+        "district": "曲阜市",
+        "pic": "https://images.qdbfg.com/jining/qufu.png",
+        "id": 4
+    },
+    {
+        "ten_fee": 17.39,
+        "fee": 1800.38,
+        "item_sum": 7,
+        "item_sum_all": 14,
+        "item_start": 6,
+        "item_finish": 1,
+        "district": "济宁高新区",
+        "pic": "https://images.qdbfg.com/jining/gaoxinqu.png",
+        "id": 12
+    },
+    {
+        "ten_fee": 13.72,
+        "fee": 1800.38,
+        "item_sum": 7,
+        "item_sum_all": 9,
+        "item_start": 2,
+        "item_finish": 0,
+        "district": "任城区",
+        "pic": "https://images.qdbfg.com/jining/rencheng.png",
+        "id": 2
+    },
+    {
+        "ten_fee": 9.26,
+        "fee": 1800.38,
+        "item_sum": 26,
+        "item_sum_all": 50,
+        "item_start": 24,
+        "item_finish": 0,
+        "district": "梁山县",
+        "pic": "https://images.qdbfg.com/jining/liangshan.png",
+        "id": 8
+    },
+    {
+        "ten_fee": 8.62,
+        "fee": 1800.38,
+        "item_sum": 17,
+        "item_sum_all": 35,
+        "item_start": 12,
+        "item_finish": 6,
+        "district": "嘉祥县",
+        "pic": "https://images.qdbfg.com/jining/jiaxiang.png",
+        "id": 10
+    },
+    {
+        "ten_fee": 6.41,
+        "fee": 1800.38,
+        "item_sum": 13,
+        "item_sum_all": 23,
+        "item_start": 7,
+        "item_finish": 3,
+        "district": "金乡县",
+        "pic": "https://images.qdbfg.com/jining/jinxiang.png",
+        "id": 5
+    },
+    {
+        "ten_fee": 5.09,
+        "fee": 1800.38,
+        "item_sum": 9,
+        "item_sum_all": 18,
+        "item_start": 8,
+        "item_finish": 1,
+        "district": "济宁经开区",
+        "pic": "https://images.qdbfg.com/jining/jingkaiqu.png",
+        "id": 13
+    },
+    {
+        "ten_fee": 4.23,
+        "fee": 1800.38,
+        "item_sum": 10,
+        "item_sum_all": 20,
+        "item_start": 8,
+        "item_finish": 2,
+        "district": "泗水县",
+        "pic": "https://images.qdbfg.com/jining/sishui.png",
+        "id": 7
+    },
+    {
+        "ten_fee": 2.34,
+        "fee": 1800.38,
+        "item_sum": 17,
+        "item_sum_all": 43,
+        "item_start": 14,
+        "item_finish": 12,
+        "district": "汶上县",
+        "pic": "https://images.qdbfg.com/jining/wenshang.png",
+        "id": 6
+    },
+    {
+        "ten_fee": 0.98,
+        "fee": 1800.38,
+        "item_sum": 9,
+        "item_sum_all": 16,
+        "item_start": 5,
+        "item_finish": 2,
+        "district": "邹城区",
+        "pic": "https://images.qdbfg.com/jining/zoucheng.png",
+        "id": 1
+    },
+    {
+        "ten_fee": 0,
+        "fee": 1800.38,
+        "item_sum": 1,
+        "item_sum_all": 2,
+        "item_start": 1,
+        "item_finish": 0,
+        "district": "太白湖新区",
+        "pic": "https://images.qdbfg.com/jining/taibaihu.png",
+        "id": 14
+    }];
 
-// Create axes
-var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-categoryAxis.dataFields.category = "country";
+
+let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
 categoryAxis.renderer.grid.template.location = 0;
-categoryAxis.renderer.minGridDistance = 20;
-categoryAxis.renderer.inside = true;
-categoryAxis.renderer.grid.template.disabled = true;
+categoryAxis.dataFields.category = "district";
+categoryAxis.renderer.minGridDistance = 80;
 
-let labelTemplate = categoryAxis.renderer.labels.template;
-labelTemplate.rotation = -90;
-labelTemplate.horizontalCenter = "left";
-labelTemplate.verticalCenter = "middle";
-labelTemplate.dy = 10; // moves it a bit down;
-labelTemplate.inside = false; // this is done to avoid settings which are not suitable when label is rotated
+let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
-var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-valueAxis.renderer.grid.template.disabled = true;
+let series = chart.series.push(new am4charts.ColumnSeries());
+series.dataFields.categoryX = "district";
+series.dataFields.valueY = "ten_fee";
+series.tooltipText = "十强产业到位资金：{valueY.value}"
+series.columns.template.strokeOpacity = 0;
 
-// Create series
-var series = chart.series.push(new am4charts.ConeSeries());
-series.dataFields.valueY = "visits";
-series.dataFields.categoryX = "country";
+chart.cursor = new am4charts.XYCursor();
 
-var columnTemplate = series.columns.template;
-columnTemplate.adapter.add("fill", (fill, target) => {
+// Add distinctive colors for each column using adapter
+series.columns.template.adapter.add("fill", (fill, target) => {
     return chart.colors.getIndex(target.dataItem.index);
-})
-
-columnTemplate.adapter.add("stroke", (stroke, target) => {
-    return chart.colors.getIndex(target.dataItem.index);
-})
+});
